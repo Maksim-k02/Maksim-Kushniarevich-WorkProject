db06b271e291f3fa3e4156af69f6b3dd3c82952d
Maksim-k02 <kushnerevich.m@gmail.com>
2021-11-11 14:12:34 +0300

    multimodule structure (model)

---
+++ model/src/main/java/com/epam/brest/model/Epmloyee.java
@@ -0,0 +1,59 @@
+package com.epam.brest.model;
+
+
+public class Epmloyee {
+    private  Integer employeeId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String salary;
+    private  Integer departmentId;
+
+    public Integer getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Integer employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+
+    public Integer getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Integer departmentId) {
+        this.departmentId = departmentId;
+    }
+}
